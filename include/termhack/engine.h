88b7b9f70f1fb5304f5d5ed7fad4b5f0de34cedb
glebundiy <gleb.shigin@gmail.com>
2025-05-25 14:01:57 +0300

    initial

---
+++ include/termhack/engine.h
@@ -0,0 +1,33 @@
+// Copyright 2024-2025 Gleb Shigin. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+#pragma once
+// termhack
+#include <termhack/core/types.h>
+#include <termhack/util/random.h>
+// stl
+#include <cstdint>
+#include <memory>
+
+namespace termhack {
+class engine {
+public:
+  engine(lock_level lock_level_setting, uint32_t player_science_skill,
+         uint64_t seed) noexcept;
+  engine() noexcept : engine(lock_level::average, 50, 0){};
+
+  ~engine();
+
+  engine(engine const &) = delete;
+  auto operator=(engine const &) -> engine & = delete;
+
+  engine(engine &&) noexcept = delete;
+  auto operator=(engine &&) noexcept -> engine & = delete;
+
+  auto process_input(input input) noexcept -> state;
+
+private:
+  struct impl;
+  std::unique_ptr<impl> pimpl_;
+};
+} // namespace termhack
---
+++ src/detail/interaction_logic.hpp
@@ -0,0 +1,49 @@
+// Copyright 2024-2025 Gleb Shigin. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+#pragma once
+// termhack
+#include <termhack/core/types.h>
+// stl
+#include <functional>
+
+// fwd
+namespace termhack::detail {
+class game_state;
+class terminal_buffer;
+class word_repository;
+} // namespace termhack::detail
+
+namespace termhack::detail::interaction_logic {
+namespace _internal {
+using rng_fref = const std::function<uint32_t()> &;
+
+auto click_at(std::size_t index, terminal_buffer &terminal,
+              word_repository &words, game_state &state,
+              rng_fref gen_next) noexcept -> click_status;
+template <class URBG>
+  requires std::uniform_random_bit_generator<std::remove_reference_t<URBG>>
+constexpr auto wrap_rng(URBG &&rng) noexcept;
+} // namespace _internal
+
+auto look_at(std::size_t index, const terminal_buffer &terminal,
+             game_state &state) noexcept -> highlight;
+
+template <class URBG>
+  requires std::uniform_random_bit_generator<std::remove_reference_t<URBG>>
+auto click_at(std::size_t index, terminal_buffer &terminal,
+              word_repository &words, game_state &state,
+              URBG &g) noexcept -> click_status {
+  return _internal::click_at(index, terminal, words, state,
+                             _internal::wrap_rng(std::forward<URBG>(g)));
+}
+
+} // namespace termhack::detail::interaction_logic
+
+namespace termhack::detail::interaction_logic::_internal {
+template <class URBG>
+  requires std::uniform_random_bit_generator<std::remove_reference_t<URBG>>
+constexpr auto wrap_rng(URBG &&rng) noexcept {
+  return [&]() { return rng(); };
+}
+} // namespace termhack::detail::interaction_logic::_internal
